c59bb32090ca008405ebaa189fa4492edc0c907d
marceloaparecidoleonel-alt <marceloaparecidoleonel@gmail.com>
2026-09-07 23:03:09 -0300

    fix: set authDomain to vercel domain to fix unauthorized-domain OAuth error

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,7 +9,7 @@
 
 const firebaseConfig = {
   apiKey:            "AIzaSyA_t97V-U1y7p005CxiiAIykWN4njUO8PM",
-  authDomain:        "teixeira-style.firebaseapp.com",
+  authDomain:        "teixeira-style.vercel.app",
   projectId:         "teixeira-style",
   storageBucket:     "teixeira-style.firebasestorage.app",
   messagingSenderId: "83677282408",
